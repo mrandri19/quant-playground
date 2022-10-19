54785bf28287d3da75c85807f582d86d199513b0
Andrea Cognolato <andrecogno@hotmail.it>
2022-10-19 14:14:53 +0200

    Improve bond.py

--- src/bond.py
+++ src/bond.py
@@ -1,15 +1,20 @@
-cash_flows = [
-    4.5,  # nov 22
-    4.5,  # may 23
-    4.5 + 100.0,  # nov 23
-]
+def main():
+    cash_flows = [
+        4.5,  # nov 22
+        4.5,  # may 23
+        4.5 + 100.0,  # nov 23
+    ]
 
-ytm = 3.4175 / 100
+    ytm = 3.4175 / 100
 
-discounts = [
-    (1 + ytm) ** -0,  # nov 22
-    (1 + ytm) ** -1,  # may 23
-    (1 + ytm) ** -2,  # nov 23
-]
+    discounts = [
+        (1 + ytm) ** -0,  # nov 22
+        (1 + ytm) ** -1,  # may 23
+        (1 + ytm) ** -2,  # nov 23
+    ]
+    price = sum(c * d for c, d in zip(cash_flows, discounts))
+    print(f"price: €{price:.3f}")
 
-print(f"€{sum(c * d for c, d in zip(cash_flows, discounts)):.3f}")
+
+if __name__ == "__main__":
+    main()
